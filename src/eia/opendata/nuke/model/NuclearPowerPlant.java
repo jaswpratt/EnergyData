0afca156753290f03cd98fa6505ac83021c3ed2e
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-11-14 20:36:54 -0500

    First commit.

---
+++ src/eia/opendata/nuke/model/NuclearPowerPlant.java
@@ -0,0 +1,23 @@
+package eia.opendata.nuke.model;
+
+public class NuclearPowerPlant {
+	public NuclearPowerPlant(){}
+	
+	private String series_id  = null;
+	private String name = null;
+	private String units  = null;
+	private String f  = null;
+	private String unitsshort  = null;
+	private String description  = null;
+	private String source  = null;
+	private String iso3166  = null;
+	private String lat  = null;
+	private String lon  = null;
+	private String geography  = null;
+	private String start  = null;
+	private String end  = null;
+	private String last_updated  = null;
+	private String latlon  = null;
+	private TreeMap data  = null;
+
+}
