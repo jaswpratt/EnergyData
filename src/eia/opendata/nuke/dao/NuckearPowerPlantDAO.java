cd2cd27bb1a521c45c56ebcd0394a26f1959a6ce
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-11-22 12:03:42 -0500

    Update NuckearPowerPlantDAO.java
    Added code

--- src/eia/opendata/nuke/dao/NuckearPowerPlantDAO.java
+++ src/eia/opendata/nuke/dao/NuckearPowerPlantDAO.java
@@ -1,7 +1,10 @@
 /**
- * 
+ *   Nuclear Outage datasource
+ *   https://api.eia.gov/category/?api_key=cb63dd5c0e63edacebf16a18f012596b&category_id=2889994
  */
 package eia.opendata.nuke.dao;
+
+import eia.opendata.nuke.model.NuclearPowerPlantModel;
 
 /**
  * @author Sabine
@@ -10,4 +13,9 @@ package eia.opendata.nuke.dao;
 public class NuckearPowerPlantDAO {
     // Nuclear Outage datasource
     // https://api.eia.gov/category/?api_key=cb63dd5c0e63edacebf16a18f012596b&category_id=2889994
+    public static final String API_KEY = "cb63dd5c0e63edacebf16a18f012596b";
+    public static final String CATEGORY_ID = "2889994";
+    
+    private NuclearPowerPlantModel nuclearPowerPlant = new NuclearPowerPlantModel():
+    
 }
