ae8f8d0825d7f840d497c2e88db0cbd254f250de
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-11-22 17:37:20 -0500

    Updated

--- src/eia/opendata/nuke/dao/NuckearPowerPlantDAO.java
+++ src/eia/opendata/nuke/dao/NuckearPowerPlantDAO.java
@@ -4,7 +4,7 @@
  */
 package eia.opendata.nuke.dao;
 
-import eia.opendata.nuke.model.NuclearPowerPlantModel;
+import eia.opendata.nuke.model.NuclearPowerPlant;
 
 /**
  * @author Sabine
@@ -16,6 +16,6 @@ public class NuckearPowerPlantDAO {
     public static final String API_KEY = "cb63dd5c0e63edacebf16a18f012596b";
     public static final String CATEGORY_ID = "2889994";
     
-    private NuclearPowerPlantModel nuclearPowerPlant = new NuclearPowerPlantModel();
+    private NuclearPowerPlant nuclearPowerPlant = new NuclearPowerPlant();
     
 }
