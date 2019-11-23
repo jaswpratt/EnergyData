844d2ffcc4b22dd60ea2fe83add37c50ba1f05d8
Jas <55251065+jaswpratt@users.noreply.github.com>
2019-11-22 19:42:16 -0500

    Renamed class file.

--- src/eia/opendata/nuke/dao/NuclearPowerPlantDAO.java
+++ src/eia/opendata/nuke/dao/NuclearPowerPlantDAO.java
@@ -10,7 +10,7 @@ import eia.opendata.nuke.model.NuclearPowerPlant;
  * @author Sabine
  *
  */
-public class NuckearPowerPlantDAO {
+public class NuclearPowerPlantDAO {
     // Nuclear Outage datasource
     // https://api.eia.gov/category/?api_key=cb63dd5c0e63edacebf16a18f012596b&category_id=2889994
     public static final String API_KEY = "cb63dd5c0e63edacebf16a18f012596b";
